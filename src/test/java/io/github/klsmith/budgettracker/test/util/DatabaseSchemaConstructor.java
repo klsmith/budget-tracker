7702857de29e132f0803866d86908059f11d9d6e
Kooper Smith <kooper.lewis.smith@gmail.com>
2019-03-12 21:46:12 -0400

    updated DatabaseSchemaConstructor comments

--- src/test/java/io/github/klsmith/budgettracker/test/util/DatabaseSchemaConstructor.java
+++ src/test/java/io/github/klsmith/budgettracker/test/util/DatabaseSchemaConstructor.java
@@ -9,20 +9,46 @@ import org.sql2o.Sql2o;
 
 import io.github.klsmith.budgettracker.sql2o.Sql2oDao;
 
+/**
+ * <p>
+ * A class used for constructing the schema for the project.
+ * </p>
+ * <p>
+ * Should be always be updated to reflect the preferred schema for the database.
+ * </p>
+ */
 public class DatabaseSchemaConstructor extends Sql2oDao {
 
     private static final String URL_KEY = "url";
     private static final String USER_KEY = "user";
     private static final String PASS_KEY = "pass";
 
+    /**
+     * Construct a new DatabaseSchemaConstructor with the given jdbc url, username,
+     * and password.
+     */
     public DatabaseSchemaConstructor(String url, String user, String pass) {
         this(new Sql2o(url, user, pass));
     }
 
+    /**
+     * Construct a new DatabaseSchemaConstructor with the given Sql2o object.
+     */
     public DatabaseSchemaConstructor(Sql2o sql2o) {
         super(sql2o);
     }
 
+    /**
+     * <p>
+     * This entry point can be used to quickly set up a new database that is NOT
+     * embedded in a test run.
+     * </p>
+     * <h3>argument syntax:</h3>
+     * 
+     * <pre>
+     * url=jdbc:mariadb://localhost:3445/test user=root pass=test123
+     * </pre>
+     */
     public static void main(String[] args) {
         final Map<String, String> map = buildArgumentMap(args);
         final String url = map.get(URL_KEY);
@@ -46,6 +72,9 @@ public class DatabaseSchemaConstructor extends Sql2oDao {
         return map;
     }
 
+    /**
+     * Create the MoneyEntry table schema.
+     */
     public void setupMoneyEntryTable(Connection connection) {
         final Query query = connection.createQuery(
                 "CREATE TABLE MoneyEntry ("
@@ -56,6 +85,9 @@ public class DatabaseSchemaConstructor extends Sql2oDao {
         query.executeUpdate();
     }
 
+    /**
+     * Create the Tag table schema.
+     */
     public void setupTagTable(Connection connection) {
         final Query query = connection.createQuery(
                 "CREATE TABLE Tag ("
@@ -65,6 +97,9 @@ public class DatabaseSchemaConstructor extends Sql2oDao {
         query.executeUpdate();
     }
 
+    /**
+     * Create the TagMoneyEntry table schema.
+     */
     public void setupTagMoneyEntryMapTable(Connection connection) {
         final Query query = connection.createQuery(
                 "CREATE TABLE TagMoneyEntry ("
